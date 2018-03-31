7719b28a95338675304b030ebde49742def019c4
Kasif Khan <kasif780780@gmail.com>
2018-03-31 10:03:35 +0530

    Add and Use the ConfigurationMessageService Class

---
+++ AspNetCoreVideo/AspNetCoreVideo/Services/ConfigurationMessageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using AspNetCoreVideo.Services;
+
+namespace AspNetCoreVideo.Services
+{
+    public class ConfigurationMessageService:IMessageService
+    {
+        private IConfiguration _configuration;
+        public ConfigurationMessageService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessage()
+        {
+            return _configuration["Message"];
+        }
+    }
+}
