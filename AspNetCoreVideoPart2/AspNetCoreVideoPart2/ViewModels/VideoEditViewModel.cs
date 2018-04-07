c3bb881d00a3773d47f0ee0c75fe0afd376eb17b
Kasif Khan <kasif780780@gmail.com>
2018-04-08 02:15:02 +0530

    Adding the VideoEditViewModel Class

---
+++ AspNetCoreVideoPart2/AspNetCoreVideoPart2/ViewModels/VideoEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreVideoPart2.ViewModels;
+
+namespace AspNetCoreVideoPart2.ViewModels
+{
+    public class VideoEditViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genres Genre { get; set; }
+    }
+}
