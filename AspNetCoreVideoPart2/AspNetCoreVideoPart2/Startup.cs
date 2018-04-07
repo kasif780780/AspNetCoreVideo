1be502d8ee5d838cdb7f85423db2c38ef31888ff
Kasif Khan <kasif780780@gmail.com>
2018-04-08 04:34:52 +0530

    Jason File

--- AspNetCoreVideoPart2/AspNetCoreVideoPart2/Startup.cs
+++ AspNetCoreVideoPart2/AspNetCoreVideoPart2/Startup.cs
@@ -36,11 +36,11 @@ namespace AspNetCoreVideoPart2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddMvc();
-            services.AddSingleton<IVideo, MockVideoData>();
             var conn = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<VideoDbContext>(options => options.UseSqlServer(conn));
+            services.AddMvc();
+            services.AddSingleton<IVideo, MockVideoData>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
