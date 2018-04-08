ef5c33362e512a1a70b58787a5833956d75c79b2
Kasif Khan <kasif780780@gmail.com>
2018-04-08 13:22:33 +0530

    Create a new instance of the User entity class and assign its Username property value from the passed-in model, below the if-statement.

--- AspNetCoreVideoPart2/AspNetCoreVideoPart2/Controllers/AccountController.cs
+++ AspNetCoreVideoPart2/AspNetCoreVideoPart2/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreVideoPart2.ViewModels;
+using AspNetCoreVideoPart2.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace AspNetCoreVideoPart2.Controllers
 {
@@ -11,6 +14,17 @@ namespace AspNetCoreVideoPart2.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var user = new User { UserName = model.UserName };
+
             return View();
         }
     }
