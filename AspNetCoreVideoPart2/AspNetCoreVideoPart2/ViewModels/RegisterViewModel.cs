f715050f3eeea1465f0742d75aa99f6940f49436
Kasif Khan <kasif780780@gmail.com>
2018-04-08 13:06:14 +0530

    Adding The Properties in RegisterViewModel

--- AspNetCoreVideoPart2/AspNetCoreVideoPart2/ViewModels/RegisterViewModel.cs
+++ AspNetCoreVideoPart2/AspNetCoreVideoPart2/ViewModels/RegisterViewModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetCoreVideoPart2.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required,MaxLength(255)]
+        public string UserName { get; set; }
+        [Required,DataType(DataType.Password)]
+        public string Password  { get; set; }
+        [DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
     }
 }
