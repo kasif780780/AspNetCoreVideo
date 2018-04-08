2d3ca7baf4ca16abc5dac0c94b3600bff614f93e
Kasif Khan <kasif780780@gmail.com>
2018-04-08 13:25:58 +0530

    To work with the user entity, you need to bring in the UserManager and the SignInManager via the constructor, using dependency injection

--- AspNetCoreVideoPart2/AspNetCoreVideoPart2/Controllers/AccountController.cs
+++ AspNetCoreVideoPart2/AspNetCoreVideoPart2/Controllers/AccountController.cs
@@ -11,6 +11,13 @@ namespace AspNetCoreVideoPart2.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         [HttpGet]
         public IActionResult Register()
         {
