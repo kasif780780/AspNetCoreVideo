1f970ad2977e0ff25c73902e3a38e45594d18a45
Kasif Khan <kasif780780@gmail.com>
2018-04-10 12:09:13 +0530

    Adding Configuartion Service in StartUp File in Data Project

--- VideoOnDemand.UI/VideoOnDemand.Data/Startup.cs
+++ VideoOnDemand.UI/VideoOnDemand.Data/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VideoOnDemand.Data.Data;
 
 namespace VideoOnDemand.Data
 {
@@ -13,8 +16,17 @@ namespace VideoOnDemand.Data
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VODContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +36,7 @@ namespace VideoOnDemand.Data
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             app.Run(async (context) =>
             {
