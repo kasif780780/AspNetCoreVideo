37cf7e150b2b41dd3e4313b0dc31b8e31d77286e
Kasif Khan <kasif780780@gmail.com>
2018-04-11 22:55:38 +0530

    Adding the Entity Classes

---
+++ VideoOnDemand.UI/VideoOnDemand.Data/Entities/UserCourse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoOnDemand.Data.Data.Entities;
+
+namespace VideoOnDemand.Data.Entities
+{
+    public class UserCourse
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
