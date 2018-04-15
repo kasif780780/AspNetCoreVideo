00d3066741b642fe57ca06ae432d498517e34c6b
Kasif Khan <kasif780780@gmail.com>
2018-04-15 23:49:07 +0530

    Adding the Service Interface and Class

--- VideoOnDemand.UI/VideoOnDemand.Data/Services/DbWriteService.cs
+++ VideoOnDemand.UI/VideoOnDemand.Data/Services/DbWriteService.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VideoOnDemand.Data.Data;
 
 namespace VideoOnDemand.Data.Services
 {
-    public class DbWriteService
+    public class DbWriteService:IDbWriteService
     {
-        
+        private VODContext _db;
+        public DbWriteService(VODContext db)
+        {
+            _db = db;
+
+        } 
         
     }
 }
