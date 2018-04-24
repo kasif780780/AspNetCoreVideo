05f854ab298f82214511cf576156347c77794211
Kasif Khan <kasif780780@gmail.com>
2018-04-24 15:40:15 +0530

    Adding Cart Class  in Project

---
+++ BookCart/BookCart/InfraStructure/Binders/CartModelBinders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookCart.Models;
+using System.Web;
+
+
+namespace BookCart.InfraStructure.Binders
+{
+    public class CartModelBinders
+    {
+        
+    }
+}
